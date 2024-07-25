b73a7e070b1cf0b926783bae9bec2fbb86f7141a
Jasmine Leger <Jasmineleger21@gmail.com>
2024-07-25 09:06:32 -0400

    updated css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
 
 const Navbar = () => {
     const navigate = useNavigate();
 
     return (
-        <div className= "navbar">
-            
-            <Link to="/" className="button is-link"> Home</Link>
-            <Link to="/projects" className="button is-link"> Projects</Link>
-            <Link to="/skills" className="button is-link"> Skills</Link>
-
+        <div className="navbar">
+            <Link to="/" className="button is-link">Home</Link>
+            <Link to="/projects" className="button is-link">Projects</Link>
+            {/* <Link to="/skills" className="button is-link">Skills</Link> */}
         </div>
     );
 };
