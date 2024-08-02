256cb08eed1f95fb778047f019f0a25e24137037
Jasmine Leger <Jasmineleger21@gmail.com>
2024-08-01 23:51:08 -0400

    updated sills, added everything on ome page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            <Link to="/" className="button is-success is-dark">Home</Link>
-            <Link to="/Aboutme" className= "button is-success is-dark"> About Me</Link>
-            <Link to="/projects" className="button is-success is-dark">Projects</Link>
-            <Link to="/Resume" className="button is-success is-dark">Resume</Link>
+            <Link to="/" className="button is-success is-dark">Welcome to My Portfolio</Link>
+            {/* <Link to="/Aboutme" className= "button is-success is-dark"> About Me</Link> */}
+            {/* <Link to="/projects" className="button is-success is-dark">Projects</Link> */}
+            {/* <Link to="/Resume" className="button is-success is-dark">Resume</Link> */}
             
         </div>
     );
